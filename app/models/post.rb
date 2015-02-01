b0c1cd069f200ff3bb840fb0467c3ce85f12b42d
Jeff Knox <knoxjeffrey@outlook.com>
2015-02-01 21:24:10 +0000

    changed method paramater name and fixed bookmarked posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :bookmarked_posts
-  has_many :users, through: :saved_posts
+  has_many :users, through: :bookmarked_posts
   has_many :votes, as: :voteable
     
   validates :title, presence: true, uniqueness: true, length: {minimum: 5}
@@ -17,40 +17,40 @@ class Post < ActiveRecord::Base
   sluggable_column :title
   
   #check if a comment has already been made by a user
-  def already_commented_by_user?(current_user)
-    !self.comments.where(["user_id = ?", current_user.id]).empty?
+  def already_commented_by_user?(the_user)
+    !self.comments.where(["user_id = ?", the_user.id]).empty?
   end
   
   ########## VOTE ########## 
   #check if a user has already voted on a post
-  def already_voted_by_user?(current_user)
-    post_vote_array(current_user).present?
+  def already_voted_by_user?(the_user)
+    post_vote_array(the_user).present?
   end
   
   #return the id of the vote after it's confirmed that a user has already voted on a post
-  def vote_object(current_user)
-    post_vote_array(current_user).first.id
+  def vote_object(the_user)
+    post_vote_array(the_user).first.id
   end
   
-  def post_vote_array(current_user)
-    self.votes.where(["user_id = ? and voteable_id = ? and voteable_type = ?", current_user.id, self.id, self.class.name])
+  def post_vote_array(the_user)
+    self.votes.where(["user_id = ? and voteable_id = ? and voteable_type = ?", the_user.id, self.id, self.class.name])
   end
   ##########  END VOTE ##########
   
   
   ########## BOOKMARK ##########
   #check if a user has already bookmarked a post
-  def already_bookmarked_by_user?(current_user)
-    post_bookmark_array(current_user).present?
+  def already_bookmarked_by_user?(the_user)
+    post_bookmark_array(the_user).present?
   end
   
   #return the id of the bookmark after it's confirmed that a user has already bookmarked on a post
-  def bookmark_object(current_user)
-    post_bookmark_array(current_user).first.id
+  def bookmark_object(the_user)
+    post_bookmark_array(the_user).first.id
   end
   
   def post_bookmark_array(current_user)
-    current_user.bookmarked_posts.where(["user_id = ? and post_id = ?", current_user.id, self.id])
+    the_user.bookmarked_posts.where(["user_id = ? and post_id = ?", the_user.id, self.id])
   end
   ##########  END BOOKMARK ##########
   
